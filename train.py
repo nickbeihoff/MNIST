8f6f171418213e3682c443bce74b2d19807172f7
nathan98789 <shrecknathan@gmail.com>
2020-04-28 15:20:13 -0500

    Uploaded from PyCharm

---
+++ train.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import sklearn
+from sklearn.preprocessing import scale
+from sklearn.datasets import load_digits
+from sklearn.cluster import KMeans
+from sklearn import metrics
+
+digits = pandas
