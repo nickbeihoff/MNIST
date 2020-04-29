be482e87d358f8e3722f6e921f99bb08f4cdc68c
nathan98789 <shrecknathan@gmail.com>
2020-04-29 16:28:05 -0500

    Uploaded from PyCharm

---
+++ math.py
@@ -0,0 +1,17 @@
+import math
+import numpy as np
+
+
+def softmax(x):
+    return np.exp(x) / np.sum(np.exp(x), axis=0)
+
+
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+x = input("X: ")
+print("Sigmoid:")
+print(sigmoid(x))
+print("Softmax:")
+print(softmax(x))
