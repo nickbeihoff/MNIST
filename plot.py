f7e7f9f2c2c46d56c5abe8ef88a2de1021ad32ff
nathan98789 <shrecknathan@gmail.com>
2020-04-29 09:49:42 -0500

    Uploaded from PyCharm

--- plot.py
+++ plot.py
@@ -2,17 +2,5 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("train.csv", sep=',')
-df = df['label']
-df = df.to_frame()
-location = []
-for i in range(0, 10):
-    search = i
-    location.append(df.loc[df.isin([search]).any(axis=1)].index.to_list())
-print(location)
-'''
-df['Location'] = location
-df.reset_index(inplace=True)
-df.columns = ['Number', 'Location']
-df.plot(kind='scatter', x='Number', y='Location')
+plt.scatter(df.index, df['label'])
 plt.show()
-'''
